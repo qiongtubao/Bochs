bb369337639d9ba4ac6f095e2b878f25d86aa19b
周国栋 <zhouguodong@zhouguodongdeMacBook-Pro.local>
2024-11-26 13:57:27 +0800

    add cp script

---
+++ bochs/latte_cp.sh
@@ -0,0 +1,6 @@
+cp misc/bximage.cpp misc/bximage.cc 
+cp iodev/hdimage/hdimage.cpp iodev/hdimage/hdimage.cc
+cp iodev/hdimage/vmware3.cpp iodev/hdimage/vmware3.cc
+cp iodev/hdimage/vmware4.cpp iodev/hdimage/vmware4.cc
+cp iodev/hdimage/vpc-img.cpp iodev/hdimage/vpc-img.cc
+cp iodev/hdimage/vbox.cpp iodev/hdimage/vbox.cc
